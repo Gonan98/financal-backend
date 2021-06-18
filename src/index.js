6a57fcaa8c65cf9de06af0a0f6f6721eccdfb74a
Andre Gonzales <andreg-16@hotmail.com>
2021-06-18 11:27:09 -0500

    Carga de data incial removido

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import config from './config';
 import initData from './data';
 
 function main() {
-    initData();
+    //initData();
     app.listen(config.port, () => {
         console.log(`Server listening on port ${config.port}`);
     });
